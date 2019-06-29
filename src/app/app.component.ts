761779258b24e4577ee04e7f501fbbeef4ed0f8e
Rajnikant Lodhi <rajnikant.paperbyte@gmaail.com>
2019-06-29 12:49:30 +0530

    add Darkmode/night mode in angular 8  project it is very nice Add a dark-mode / night-mode to your website in a few seconds, This library uses the css mix-blend-mode in order to bring Dark-mode to any of your websites. Just copy paste the snippet and you will get a widget to turn on and off the dark-mode. You can also use it without the widget programmatically. The plugin is lightweight, built in VanillaJS. It also uses localstorage by default so your last setting will be remembered !

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import Darkmode from 'darkmode-js';
+new Darkmode().showWidget();
+var options = {
+  bottom: '64px', // default: '32px'
+  right: 'unset', // default: '32px'
+  left: '32px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '🌓' // default: ''
+}
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+
+export class AppComponent {
+  title = 'darkmode';
+
+}
